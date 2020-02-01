483d5858f195418300f5f207b9a48cdb085958d4
xuzhang0636 <363854939@qq.com>
2020-02-01 17:04:16 +0800

    first commit

---
+++ server/models/Articles.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const {
+    Schema
+} = mongoose;
+
+const ArticlesSchema = new Schema({
+    title: String,
+    body: String,
+    author: String
+}, {
+    timestamps: true
+});
+
+ArticlesSchema.method.toJSON = function () {
+    return {
+        _id: this._id,
+        title: this.title,
+        body: this.body,
+        author: this.author,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt
+    };
+};
+
+mongoose.model('Articles', ArticlesSchema);
